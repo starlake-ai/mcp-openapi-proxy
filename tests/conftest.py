30e1e5324cca537806141289003847998bf228aa
Matthew Hand <matthewhandau@gmail.com>
2025-03-07 20:13:51 +1100

    test: add conftest file

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import os
+import pytest
+import sys
+import hashlib
+from dotenv import load_dotenv
+
+# Load .env once at module level
+load_dotenv()
+
+@pytest.fixture(scope="function", autouse=True)
+def reset_env_and_module(request):
+    # Preserve original env, only tweak OPENAPI_SPEC_URL-related keys
+    original_env = os.environ.copy()
+    test_name = request.node.name
+    env_key = f"OPENAPI_SPEC_URL_{hashlib.md5(test_name.encode()).hexdigest()[:8]}"
+    # Clear only OPENAPI_SPEC_URL-related keys
+    for key in list(os.environ.keys()):
+        if key.startswith("OPENAPI_SPEC_URL"):
+            del os.environ[key]
+    os.environ["DEBUG"] = "true"
+    # Reload server_fastmcp
+    if 'mcp_openapi_proxy.server_fastmcp' in sys.modules:
+        del sys.modules['mcp_openapi_proxy.server_fastmcp']
+    yield env_key
+    # Restore original env
+    os.environ.clear()
+    os.environ.update(original_env)
